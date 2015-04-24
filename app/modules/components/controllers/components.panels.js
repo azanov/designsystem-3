0fe07a1f92a67b6483a5eee38965cc3a01177e98
danielcollette <daniel.collette@pb.com>
2015-04-24 15:22:09 -0400

    fixed miller columns
    Signed-off-by: danielcollette <daniel.collette@pb.com>

--- app/modules/components/controllers/components.panels.js
+++ app/modules/components/controllers/components.panels.js
@@ -9,17 +9,17 @@
     _this.people = PeopleResolve;
 
     _this.millercolumn = {
-        //data: millerColumn.data.versionInfos,
-        data: MillerResolve.versionInfos,
+        data: MillerResolve,
         level1: null,
         level2: null
       };
 
     _this.getSublevel1 = function(index) {
-      _this.millercolumn.level1 = _this.MillerResolve[index].versionInfos;
+      _this.millercolumn.level1 = _this.millercolumn.data.versionInfos[index].versionInfos;
       _this.millercolumn.dataSelected = index;
       _this.millercolumn.level2 = null;
       _this.millercolumn.level1Selected = null;
+
       //$log.log(_this.millercolumn.level1);
     };
 
