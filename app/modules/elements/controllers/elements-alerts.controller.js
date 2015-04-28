86345cc5cd2c4006d29cedb47e3ddd9a861ab7a4
Steve Lombardi <smlombardi@gmail.com>
2015-04-28 14:05:18 -0400

    fix progress bars
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/elements/controllers/elements-alerts.controller.js
+++ app/modules/elements/controllers/elements-alerts.controller.js
@@ -14,12 +14,20 @@
       messageClass: 'text-success'
     };
 
-    _this.showToastrAlert = function() {
-      toastr.error('That email is not recognized.', 'Sorry');
-    };
+    _this.toast = {
+      danger: function() {
+        toastr.error('It is dangerous to remain here. You must leave within two days.', 'Message as follows:');
+      },
+      success:  function() {
+        toastr.success('I am completely operational, and all my circuits are functioning perfectly.');
+      },
+      info:  function() {
+        toastr.info('The 9000 series is the most reliable computer ever made.');
+      },
+      warning:  function() {
+        toastr.warning('Just what do you think you\'re doing, Dave?');
+      }
 
-    _this.showToastrSuccess = function() {
-      toastr.success('An email has been sent to you.', 'Excellent');
     };
 
   });
