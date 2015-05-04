6636dc59327610db5d5d5967031121a58e80b909
Steve Lombardi <smlombardi@gmail.com>
2015-05-04 13:21:18 -0400

    pages section done
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/notification/notification.routes.js
+++ app/modules/notification/notification.routes.js
@@ -5,7 +5,6 @@
   angular.module('pb.ds.notification').config(function($stateProvider) {
     $stateProvider.state('notification', {
       abstract: true,
-      url: '/notification',
       templateUrl: 'modules/notification/templates/notification.html',
       controller: 'NotificationController as notification',
       data: {
@@ -16,31 +15,31 @@
     })
 
     .state('notification.toastr', {
-      url: '/toastr',
+      url: '/notification/toastr',
       templateUrl: 'modules/notification/templates/notification-toastr.html',
       controller: 'ToastrController as toastr'
     })
 
     .state('notification.spinners', {
-      url: '/spinners',
+      url: '/notification/spinners',
       templateUrl: 'modules/notification/templates/notification-spinners.html',
       controller: 'SpinnersController as spinners'
     })
 
     .state('notification.loadingbar', {
-      url: '/loadingbar',
+      url: '/notification/loadingbar',
       templateUrl: 'modules/notification/templates/notification-loadingbar.html',
       controller: 'LoadingBarController as loadingbar'
     })
 
     .state('notification.alerts', {
-      url: '/alerts',
+      url: '/notification/alerts',
       templateUrl: 'modules/notification/templates/notification-alerts.html',
       controller: 'AlertsController as alerts'
     })
 
     .state('notification.headermessage', {
-      url: '/headermessage',
+      url: '/notification/headermessage',
       templateUrl: 'modules/notification/templates/notification-header-message.html',
       controller: 'HeaderMessageController as message'
     });
--- app/modules/pages/controllers/under-maintenance.controller.js
+++ app/modules/pages/controllers/under-maintenance.controller.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  angular.module('pb.ds.pages').controller('ForgotPasswordController', function($log) {
+  angular.module('pb.ds.pages').controller('UnderMaintentanceController', function($log) {
 
     var _this = this;
 
