4c13598abc8f25da4be8a4867a89d00464a45f47
Steve Lombardi <smlombardi@gmail.com>
2015-05-10 17:10:34 -0400

    switched modals to ui-bs
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/notification/controllers/notification-alerts.controller.js
+++ app/modules/notification/controllers/notification-alerts.controller.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  angular.module('pb.ds.notification').controller('AlertsController', function($log, toastr) {
+  angular.module('pb.ds.notification').controller('AlertsController', function($log, toastr, $modal) {
 
     var _this = this;
 
@@ -22,12 +22,48 @@
         toastr.success('I am completely operational, and all my circuits are functioning perfectly.');
       },
       info:  function() {
-        toastr.info('The 9000 series is the most reliable computer ever made.');
+        toastr.info('The 9000 series is the most reliable computer ever madeelements.');
       },
       warning:  function() {
         toastr.warning('Just what do you think you\'re doing, Dave?');
       }
 
+    };
+
+    _this.modals = {
+      simple: function() {
+        $modal.open({
+          templateUrl: 'modules/notification/templates/modals/notification-simple.html',
+          controller: 'BootstrapUiSimpleModalController'
+        });
+      },
+      modal: function() {
+        $modal.open({
+          templateUrl: 'modules/notification/templates/modals/notification-modal.html',
+          controller: 'BootstrapUiModalController',
+          keyboard: false,
+          backdrop: 'static'
+        });
+      },
+      prompt: function() {
+        $modal.open({
+          templateUrl: 'modules/notification/templates/modals/notification-prompt.html',
+          controller: 'BootstrapUiPromptModalController',
+          keyboard: false,
+          backdrop: 'static'
+        }).result.then(function(fullname) {
+          _this.fullname = fullname;
+        });
+      },
+      draggable: function() {
+        $modal.open({
+          windowTemplateUrl: 'modules/notification/templates/modals/notification-draggable-template.html',
+          templateUrl: 'modules/notification/templates/modals/notification-draggable.html',
+          controller: 'BootstrapUiDraggableModalController',
+          keyboard: false,
+          backdrop: 'static'
+        });
+      }
     };
 
   });
