1a42d9543afe3f6d817e8d87647a47d952b1cf7f
danielcollette <daniel.collette@pb.com>
2015-06-08 14:33:13 -0400

    first commit of the datatable page
    Signed-off-by: danielcollette <daniel.collette@pb.com>

--- app/modules/tables/controllers/tables-datatable.controller.js
+++ app/modules/tables/controllers/tables-datatable.controller.js
@@ -2,9 +2,12 @@
 
   'use strict';
 
-  angular.module('pb.ds.tables').controller('TablesDatatableController', function($log) {
+  angular.module('pb.ds.tables').controller('TablesDatatableController',
+  function($log, MockDataFactory) {
 
     var _this = this;
+
+    _this.data = MockDataFactory.query({filename: 'people_10'});
 
   });
 
