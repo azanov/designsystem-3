db6edfb295abbd76786e39c2ee50737202283ee2
danielcollette <daniel.collette@pb.com>
2015-06-09 11:06:12 -0400

    add limited datatables functionality
    Signed-off-by: danielcollette <daniel.collette@pb.com>

--- app/modules/tables/controllers/tables-datatable.controller.js
+++ app/modules/tables/controllers/tables-datatable.controller.js
@@ -9,6 +9,14 @@
 
     _this.data = MockDataFactory.query({filename: 'people_10'});
 
+    _this.sortType = 'first_name';
+    _this.sortReverse  = false;
+
+    _this.sort = function(key) {
+      _this.sortType = key;
+      _this.sortReverse = !_this.sortReverse;
+    };
+
   });
 
 })();
