7639f4b62f9b2d6aaed01c8195f3381027bc3536
Steve Lombardi <smlombardi@gmail.com>
2015-07-14 11:28:58 -0400

    first two pages of globalization
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/globalization/globalization.routes.js
+++ app/modules/globalization/globalization.routes.js
@@ -4,15 +4,51 @@
 
   angular.module('pb.ds.globalization').config(function($stateProvider) {
     $stateProvider.state('globalization', {
-      url: '/globalization',
-      templateUrl: 'modules/globalization/templates/globalization.html',
-      controller: 'GlobalizationController as globalization',
-      data: {
-        pageTitle: 'Globalization',
-        access: 'public',
-        bodyClass: 'globalization'
-      }
-    });
-  });
+        // abstract: true,
+        url: '/globalization',
+        templateUrl: 'modules/globalization/templates/globalization.html',
+        controller: 'GlobalizationController as globalization',
+        data: {
+          pageTitle: 'Globalization',
+          access: 'public',
+          bodyClass: 'globalization'
+        }
+      })
+      .state('globalization.overview', {
+        url: '/overview',
+        templateUrl: 'modules/globalization/templates/globalization-overview.html',
+        controller: 'GlobalizationController as globalization'
+      })
+      .state('globalization.pseudo', {
+        url: '/pseudolocalization',
+        templateUrl: 'modules/globalization/templates/globalization-pseudolocalization.html',
+        controller: 'GlobalizationController as globalization'
+      })
+      .state('globalization.preferences', {
+        url: '/preferences',
+        templateUrl: 'modules/globalization/templates/globalization-preferences.html',
+        controller: 'GlobalizationController as globalization'
+      })
+      .state('globalization.strings', {
+        url: '/strings',
+        templateUrl: 'modules/globalization/templates/globalization-strings.html',
+        controller: 'GlobalizationController as globalization'
+      })
+      .state('globalization.ui', {
+        url: '/uiconsiderations',
+        templateUrl: 'modules/globalization/templates/globalization-uiconsiderations.html',
+        controller: 'GlobalizationController as globalization'
+      })
+      .state('globalization.graphics', {
+        url: '/graphics',
+        templateUrl: 'modules/globalization/templates/globalization-graphics.html',
+        controller: 'GlobalizationController as globalization'
+      })
+      .state('globalization.bidirectional', {
+        url: '/bidirectional',
+        templateUrl: 'modules/globalization/templates/globalization-bidirectional.html',
+        controller: 'GlobalizationController as globalization'
+      });
 
+  });
 })();
