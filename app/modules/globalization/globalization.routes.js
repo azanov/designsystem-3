66f5167debb4a5f5fcbc333a8417d7804a38d33b
danielcollette <daniel.collette@pb.com>
2015-07-24 08:27:54 -0400

    add angulat translate route
    Signed-off-by: danielcollette <daniel.collette@pb.com>

--- app/modules/globalization/globalization.routes.js
+++ app/modules/globalization/globalization.routes.js
@@ -14,40 +14,53 @@
           bodyClass: 'globalization'
         }
       })
+
       .state('globalization.overview', {
         url: '/overview',
         templateUrl: 'modules/globalization/templates/globalization-overview.html',
         controller: 'GlobalizationController as globalization'
       })
+
       .state('globalization.pseudo', {
         url: '/pseudolocalization',
         templateUrl: 'modules/globalization/templates/globalization-pseudolocalization.html',
         controller: 'GlobalizationController as globalization'
       })
+
       .state('globalization.preferences', {
         url: '/preferences',
         templateUrl: 'modules/globalization/templates/globalization-preferences.html',
         controller: 'GlobalizationController as globalization'
       })
+
       .state('globalization.strings', {
         url: '/strings',
         templateUrl: 'modules/globalization/templates/globalization-strings.html',
         controller: 'GlobalizationController as globalization'
       })
+
       .state('globalization.ui', {
         url: '/uiconsiderations',
         templateUrl: 'modules/globalization/templates/globalization-uiconsiderations.html',
         controller: 'GlobalizationController as globalization'
       })
+
       .state('globalization.graphics', {
         url: '/graphics',
         templateUrl: 'modules/globalization/templates/globalization-graphics.html',
         controller: 'GlobalizationController as globalization'
       })
+
       .state('globalization.bidirectional', {
         url: '/bidirectional',
         templateUrl: 'modules/globalization/templates/globalization-bidirectional.html',
         controller: 'GlobalizationController as globalization'
+      })
+
+      .state('globalization.angulartranslate', {
+        url: '/angulartranslate',
+        templateUrl: 'modules/globalization/templates/globalization-angular-translate.html',
+        controller: 'AngularTranslateController as translate'
       });
 
   });
