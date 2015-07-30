dfe9bb7e798969fc500878f691f429545110e7df
danielcollette <daniel.collette@pb.com>
2015-07-30 08:31:20 -0400

    add -xs spacer class
    Signed-off-by: danielcollette <daniel.collette@pb.com>

--- app/modules/helpers/controllers/helpers.controller.js
+++ app/modules/helpers/controllers/helpers.controller.js
@@ -8,11 +8,11 @@
 
     _this.spacers = {
       top: {
-        values: ['no top spacer', 'spacer-top-sm', 'spacer-top-md', 'spacer-top-lg', 'spacer-top-xl'],
+        values: ['no top spacer', 'spacer-top-xs', 'spacer-top-sm', 'spacer-top-md', 'spacer-top-lg', 'spacer-top-xl'],
         selected: 'spacer-top-sm'
       },
       bottom: {
-        values: ['no bottom spacer', 'spacer-bottom-sm', 'spacer-bottom-md', 'spacer-bottom-lg', 'spacer-bottom-xl'],
+        values: ['no bottom spacer', 'spacer-bottom-xs','spacer-bottom-sm', 'spacer-bottom-md', 'spacer-bottom-lg', 'spacer-bottom-xl'],
         selected: 'spacer-bottom-sm'
       }
     };
