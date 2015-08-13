ad1624bc1afada8e79aaaa237ea5a3b26dca875d
Steve Lombardi <smlombardi@gmail.com>
2015-08-13 14:51:47 -0400

    fixed the goddamned brand module
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/brand/brand.routes.js
+++ app/modules/brand/brand.routes.js
@@ -6,13 +6,48 @@
     $stateProvider.state('brand', {
       url: '/brand',
       templateUrl: 'modules/brand/templates/brand.html',
-      controller: 'BrandController as brand',
+      controller: 'BrandController as bc',
+      data: {
+        pageTitle: 'Wordpress',
+        access: 'public',
+        bodyClass: 'brand'
+      }
+    })
+
+    .state('brand.styleguide', {
+      url: '/styleguide',
+      templateUrl: 'modules/brand/templates/brand-styleguide.html',
+      controller: 'StyleGuideController as style',
+      data: {
+        pageTitle: 'Wordpress',
+        access: 'public',
+        bodyClass: 'brand'
+      }
+    })
+
+    .state('brand.voice', {
+      url: '/voice',
+      templateUrl: 'modules/brand/templates/brand-voice.html',
+      controller: 'BrandController as bc',
+      data: {
+        pageTitle: 'Wordpress',
+        access: 'public',
+        bodyClass: 'brand'
+      }
+    })
+
+
+    .state('brand.personality', {
+      url: '/personality',
+      templateUrl: 'modules/brand/templates/brand-personality.html',
+      controller: 'BrandController as bc',
       data: {
         pageTitle: 'Wordpress',
         access: 'public',
         bodyClass: 'brand'
       }
     });
+
   });
 
 })();
