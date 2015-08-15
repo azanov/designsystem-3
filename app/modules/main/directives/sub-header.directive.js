cb4ff1141f1b8e0511815aecd68c2535aa28094e
Steve Lombardi <smlombardi@gmail.com>
2015-08-15 10:53:24 -0400

    form fields
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/main/directives/sub-header.directive.js
+++ app/modules/main/directives/sub-header.directive.js
@@ -6,7 +6,8 @@
     return {
       restrict: 'E',
       templateUrl: 'modules/main/templates/sub-header.html',
-      controller: 'SubHeaderController as subHeader',
+      controller: 'SubHeaderController as subheader',
+
       transclude: true,
       link: function(scope, el, attrs, ctrl, transclude) {
         el.find('.jump-menu').append(transclude());
