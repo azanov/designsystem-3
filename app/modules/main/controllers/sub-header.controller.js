49ec5af0b62ed20500c0b552da343d3d68e5ca89
danielcollette <daniel.collette@pb.com>
2015-08-17 13:15:46 -0400

    subnav anchor links working
    Signed-off-by: danielcollette <daniel.collette@pb.com>

--- app/modules/main/controllers/sub-header.controller.js
+++ app/modules/main/controllers/sub-header.controller.js
@@ -2,11 +2,23 @@
 
   'use strict';
 
-  angular.module('app').controller('SubHeaderController', function($log) {
+  angular.module('app').controller('SubHeaderController', function($log, $location, $anchorScroll) {
 
     var _this = this;
 
+    _this.linkTo = function(target) {
+      $log.debug($location, target);
+      $anchorScroll.yOffset = 150;
 
+      $location.hash(target);
+      $anchorScroll();
+    };
+
+  });
+
+  angular.module('app')
+  .run(function($anchorScroll) {
+    $anchorScroll.yOffset = 150;   // always scroll by 150 extra pixels
   });
 
 })();
