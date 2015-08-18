960377eed4edb49a827775898b52c4a6f665d8bc
danielcollette <daniel.collette@pb.com>
2015-08-18 10:43:17 -0400

    update resources nav
    Signed-off-by: danielcollette <daniel.collette@pb.com>

--- app/modules/resources/resources.routes.js
+++ app/modules/resources/resources.routes.js
@@ -5,39 +5,27 @@
   angular.module('pb.ds.resources').config(function($stateProvider) {
     $stateProvider.state('resources', {
       url: '/resources',
+      abstract: true,
       templateUrl: 'modules/resources/templates/resources.html',
       controller: 'ResourcesController as resources',
       data: {
-        pageTitle: 'Principles - Design Philosophy',
+        pageTitle: 'Resources',
         access: 'public',
         bodyClass: 'resources'
       }
     })
 
-    .state('resources.designers', {
-      url: '/designers',
-      templateUrl: 'modules/resources/templates/resources-designers.html',
-      controller: 'ResourcesController as resources',
-      data: {
-        pageTitle: 'Resources for Designers',
-        access: 'public',
-        bodyClass: 'resources'
-      }
-    })
-
-    .state('resources.developers', {
-      url: '/developers',
-      templateUrl: 'modules/resources/templates/resources-developers.html',
-      controller: 'ResourcesController as resources',
-      data: {
-        pageTitle: 'Resources for Developers',
-        access: 'public',
-        bodyClass: 'resources'
+    .state('resources.page', {
+      url: '',
+      views: {
+        developers: {
+          templateUrl: 'modules/resources/templates/resources-developers.html'
+        },
+        designers: {
+          templateUrl: 'modules/resources/templates/resources-designers.html'
+        }
       }
     });
-
-
-
 
   });
 
