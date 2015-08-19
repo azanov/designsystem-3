6dd96bfce55ac56a91033e8d00a6fb0dec738098
danielcollette <daniel.collette@pb.com>
2015-08-19 14:53:26 -0400

    move font sample to typography
    Signed-off-by: danielcollette <daniel.collette@pb.com>

--- app/modules/foundation/controllers/foundation-typography.controller.js
+++ app/modules/foundation/controllers/foundation-typography.controller.js
@@ -55,6 +55,34 @@
       { desc:'Column Headers', color:'#2E2E2E'}
     ];
 
+    _this.fonts = [
+      {
+        fontName: 'Precision Sans Light',
+        font: 'PrecisionSans_W_Lt'
+      }, {
+        fontName: 'Precision Sans Light Italic',
+        font: 'PrecisionSans_W_LtIt'
+      }, {
+        fontName: 'Precision Sans Regular',
+        font: 'PrecisionSans_W_Rg'
+      }, {
+        fontName: 'Precision Sans Regular Italic',
+        font: 'PrecisionSans_W_RgIt'
+      }, {
+        fontName: 'Precision Sans Medium',
+        font: 'PrecisionSans_W_Md'
+      }, {
+        fontName: 'Precision Sans Medium Italic',
+        font: 'PrecisionSans_W_MdIt'
+      }, {
+        fontName: 'Precision Sans Bold',
+        font: 'PrecisionSans_W_Bd'
+      }, {
+        fontName: 'Precision Sans Bold Italic',
+        font: 'PrecisionSans_W_BdIt'
+      }
+    ];
+
 
 
 
