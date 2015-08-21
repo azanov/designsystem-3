e342952721e6745de97843b28f19498b4dbbd667
danielcollette <daniel.collette@pb.com>
2015-08-21 15:56:51 -0400

    landing page revisions
    Signed-off-by: danielcollette <daniel.collette@pb.com>

--- app/modules/patterns/controllers/welcome.controller.js
+++ app/modules/patterns/controllers/welcome.controller.js
@@ -7,22 +7,22 @@
     var _this = this;
 
 
-    _this.showHeaderFooter = false;
-
-    _this.sendUsername = function() {
-      if (_this.emailAddress !== null) {
-        _this.isValid = true;
-        _this.isInvalid = false;
-
-      } else {
-        _this.isInvalid = true;
-        _this.isValid = false;
-      }
-    };
-
-    _this.setPassword = function() {
-      $state.go('dashboard');
-    };
+    // _this.showHeaderFooter = false;
+    //
+    // _this.sendUsername = function() {
+    //   if (_this.emailAddress !== null) {
+    //     _this.isValid = true;
+    //     _this.isInvalid = false;
+    //
+    //   } else {
+    //     _this.isInvalid = true;
+    //     _this.isValid = false;
+    //   }
+    // };
+    //
+    // _this.setPassword = function() {
+    //   $state.go('dashboard');
+    // };
 
 
   });
