f8c64510e504e6cc57b4016c6ed2dad1af0b0927
danielcollette <daniel.collette@pb.com>
2015-09-22 20:06:41 -0400

    add daterangepicker back in, start color changes
    Signed-off-by: danielcollette <daniel.collette@pb.com>

--- app/modules/elements/controllers/elements-inputs.controller.js
+++ app/modules/elements/controllers/elements-inputs.controller.js
@@ -124,6 +124,44 @@
 
 
 
+    _this.daterangepicker = {
+      date: {
+        startDate: moment().startOf('year'),
+        endDate: moment().endOf('month')
+      },
+      options: {
+        singleDatePicker: false,
+        opens: 'center',
+        ranges: {
+          Today: [moment(), moment()],
+          'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+          'Month to date': [moment().startOf('month'), moment()],
+          'Year to date': [moment().startOf('year'), moment()]
+        }
+      },
+      displayDate: function(start, end) {
+
+        var startDate = start || _this.table.daterangepicker.date.startDate;
+        var endDate = end || _this.table.daterangepicker.date.endDate;
+
+        var dateDiff = endDate.diff(startDate, 'days');
+        var result = '';
+
+        if (dateDiff === 0) {
+          result = 'Today ' + moment(startDate).format('MM/DD/YYYY');
+        }
+        else if (dateDiff === 6) {
+          result = 'Last 7 days';
+        }
+        else {
+          result = 'From ' + moment(startDate).format('MM/DD/YYYY') + ' to ' + moment(endDate).format('MM/DD/YYYY');
+        }
+
+        return result;
+      }
+    };
+
+
     _this.customRange = {
 
       items: [
