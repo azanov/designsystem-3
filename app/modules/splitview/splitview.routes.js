f5ae5fce5f9a79190b50bb0ae615d807494e17b9
Steve Lombardi <smlombardi@gmail.com>
2015-09-26 09:32:06 -0400

    split views
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/splitview/splitview.routes.js
+++ app/modules/splitview/splitview.routes.js
@@ -3,9 +3,9 @@
   'use strict';
 
   angular.module('pb.ds.splitview').config(function($stateProvider) {
-    $stateProvider.state('splitview', {
+    $stateProvider.state('splitview-examples', {
       url: '/splitview',
-      templateUrl: 'modules/splitview/templates/splitview-bgphoto.html',
+      templateUrl: 'modules/splitview/templates/splitview-one.html',
       controller: 'SplitviewController as splitview',
       resolve: {
         NavigationResolve: function($log, MockDataFactory) {
@@ -18,60 +18,34 @@
         bodyClass: 'splitview'
       }
     })
-    .state('splitview-bgphoto', {
-      url: '/splitview/bg-image',
-      templateUrl: 'modules/splitview/templates/splitview-bgphoto.html',
+    .state('splitview-one', {
+      url: '/splitview/one',
+      templateUrl: 'modules/splitview/templates/splitview-one.html',
       data: {
         pageTitle: 'Split Views',
         access: 'public',
-        bodyClass: 'splitview splitview-bgImg'
+        bodyClass: 'splitview'
       }
     })
-    .state('splitview-threeCol', {
-      url: '/splitview/threeCol',
-      templateUrl: 'modules/splitview/templates/splitview-threeCol.html',
+    .state('splitview-two', {
+      url: '/splitview/two',
+      templateUrl: 'modules/splitview/templates/splitview-two.html',
       data: {
         pageTitle: 'Split Views',
         access: 'public',
-        bodyClass: 'splitview splitview-threeCol'
-      }
-    })
-    .state('splitview-form', {
-      url: '/splitview/form',
-      templateUrl: 'modules/splitview/templates/splitview-form.html',
-      data: {
-        pageTitle: 'Split Views',
-        access: 'public',
-        bodyClass: 'splitview splitview-form'
-      }
-    })
-    .state('splitview-product', {
-      url: '/splitview/product',
-      templateUrl: 'modules/splitview/templates/splitview-product.html',
-      data: {
-        pageTitle: 'Split Views',
-        access: 'public',
-        bodyClass: 'splitview splitview-product'
-      }
-    })
-    .state('splitview-info', {
-      url: '/splitview/info',
-      templateUrl: 'modules/splitview/templates/splitview-info.html',
-      data: {
-        pageTitle: 'Split Views',
-        access: 'public',
-        bodyClass: 'splitview splitview-info'
-      }
-    })
-    .state('splitview-slider', {
-      url: '/splitview/slider',
-      templateUrl: 'modules/splitview/templates/splitview-slider.html',
-      data: {
-        pageTitle: 'Split Views',
-        access: 'public',
-        bodyClass: 'splitview splitview-slider'
+        bodyClass: 'splitview'
       }
     });
+
+
+
+
+
+
+
+
+
+
   });
 
 })();
