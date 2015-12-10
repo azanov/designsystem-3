4d3d95564f78f97e999562b8a47debf8162b9027
Steve Lombardi <smlombardi@gmail.com>
2015-12-10 12:20:10 -0500

    added ui-bootstrap popovers and tooltips
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/help/controllers/help.controller.js
+++ app/modules/help/controllers/help.controller.js
@@ -2,18 +2,22 @@
 
   'use strict';
 
-  angular.module('pb.ds.help').controller('HelpController', function($log, $uibModal, $scope) {
+  angular.module('pb.ds.help').controller('HelpController', function($log, $uibModal, $scope, $sce) {
 
     var _this = this;
 
     // popovers
     _this.dynamicPopover = {
       content: 'Hello, World!',
-      templateUrl: 'myPopoverTemplate.html',
+      templateUrl: 'modules/help/templates/popover_template.html',
       title: 'Title'
     };
 
-    
+    _this.dynamicTooltip = 'Hello, World!';
+    _this.dynamicTooltipText = 'dynamic';
+    _this.htmlTooltip = $sce.trustAsHtml('I\'ve been made <b>bold</b>!');
+
+
 
 
 
