0819253af4102dedf2697a9ba159de2a07a8b1b4
danielcollette <daniel.collette@pb.com>
2016-01-20 12:17:40 -0500

    add another example, use controller code
    Signed-off-by: danielcollette <daniel.collette@pb.com>

--- app/modules/elements/controllers/elements-containers.controller.js
+++ app/modules/elements/controllers/elements-containers.controller.js
@@ -6,6 +6,99 @@
 
     var _this = this;
 
+    _this.imageGallery = {
+      items: [1,2,3,4,5,6,7,8,9,10,11,12],
+      settings: {
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          }
+        ]
+      }
+    }
+
+    _this.itemGallery = {
+      items: [
+        {
+          title: 'Self Assessment',
+          icon: 'fa-search',
+          description: 'Lorem ipsum dolor'
+        },
+        {
+          title: 'Capital Gains Tax',
+          icon: 'fa-umbrella',
+          description: 'Lorem ipsum dolor'
+        },
+        {
+          title: 'Child Benefit',
+          icon: 'fa-wifi',
+          description: 'Lorem ipsum dolor'
+        },
+        {
+          title: 'Corporation Tax',
+          icon: 'fa-user',
+          description: 'Lorem ipsum dolor'
+        },
+        {
+          title: 'Income Tax',
+          icon: 'fa-shopping-cart',
+          description: 'Lorem ipsum dolor'
+        },
+        {
+          title: 'Inheritance Tax',
+          icon: 'fa-user',
+          description: 'Lorem ipsum dolor'
+        },
+        {
+          title: 'Premium Tax',
+          icon: 'fa-glass',
+          description: 'Lorem ipsum dolor'
+        },
+        {
+          title: 'National Insurance',
+          icon: 'fa-tint',
+          description: 'Lorem ipsum dolor'
+        }
+      ],
+      settings: {
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      }
+    }
+
   });
 
 })();
