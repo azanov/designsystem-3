d03e0069170c20357ddabc7dcd566110f321d8a6
Steve Lombardi <smlombardi@gmail.com>
2016-02-09 10:08:03 -0500

    Added pageslide directive & burger menu section
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/hamburger/controllers/hamburger.controller.js
+++ app/modules/hamburger/controllers/hamburger.controller.js
@@ -2,6 +2,7 @@
 
   'use strict';
 
+<<<<<<< dc57317ef5e135e4383b0eb61fbedc1f108317c3
   angular.module('pb.ds.splitview').controller('HamburgerController', function($log) {
 
     var _this = this;
@@ -86,6 +87,79 @@
   angular.element(document).ready(function() {
 
     c = angular.element(document.querySelector('#controller-demo')).scope();
+=======
+  angular.module('pb.ds.splitview').controller('SplitviewController', function($log) {
+
+    var _this = this;
+
+    _this.leftnav =  [{
+       'label': 'Appetizers',
+       'value': '',
+       'toggle': 'dropdown',
+       'children': [{
+         'label': 'Hot Wings',
+         'value': ''
+       }, {
+         'label': 'Nachos',
+         'value': ''
+       }, {
+         'label': 'Bruscheta',
+         'value': ''
+       }, {
+         'label': 'Chicken Fingers',
+         'value': ''
+       }]
+     }, {
+       'label': 'Salads',
+       'value': '',
+       'toggle': 'dropdown',
+       'children': [{
+         'label': 'Cobb',
+         'value': ''
+       }, {
+         'label': 'Chef',
+         'value': ''
+       }, {
+         'label': 'American',
+         'value': ''
+       }, {
+         'label': 'Greek',
+         'value': '',
+         'className': 'popovers'
+       }, {
+         'label': 'Tomato Basil',
+         'value': ''
+       }]
+     }, {
+       'label': 'Desserts',
+       'value': '',
+       'toggle': 'dropdown',
+       'children': [{
+         'label': 'Ice Cream',
+         'value': ''
+       }, {
+         'label': 'Rice Pudding',
+         'value': ''
+       }, {
+         'label': 'Creme Brulee',
+         'value': ''
+       }, {
+         'label': 'Sorbet',
+         'value': ''
+       }, {
+         'label': 'Cake',
+         'value': ''
+       }, {
+         'label': 'Yodels',
+         'value': ''
+       }, {
+         'label': 'Cannoli',
+         'value': ''
+       }]
+     }];
+
+
+>>>>>>> Added pageslide directive & burger menu section
   });
 
 })();
