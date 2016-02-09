4bb66a8cb3df44bf3c435780e57307aad59a1700
Steve Lombardi <smlombardi@gmail.com>
2016-02-09 10:08:03 -0500

    Added pageslide directive & burger menu section
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

---
+++ app/modules/hamburger/hamburger.routes.js
@@ -0,0 +1,96 @@
+(function() {
+
+  'use strict';
+
+  angular.module('pb.ds.hamburger').config(function($stateProvider) {
+    $stateProvider.state('hamburger', {
+      abstract: true,
+      url: '/hamburger',
+      templateUrl: 'modules/hamburger/templates/hamburger.html',
+      controller: 'SplitviewController as hamburger'
+    })
+
+    .state('hamburger.overview', {
+      url: '/overview',
+      templateUrl: 'modules/hamburger/templates/hamburger-overview.html',
+      data: {
+        pageTitle: 'Full-Time Hamburger Menu - Overview',
+        access: 'public',
+        bodyClass: ''
+      }
+    })
+
+    .state('hamburger.one', {
+      url: '/hamburger/one',
+      templateUrl: 'modules/hamburger/templates/hamburger-one.html',
+      data: {
+        pageTitle: 'Full-Time Hamburger Menu',
+        access: 'public',
+        bodyClass: 'hamburger hamburger-filled'
+      }
+    })
+
+    .state('hamburger.two', {
+      url: '/hamburger/two',
+      templateUrl: 'modules/hamburger/templates/hamburger-two.html',
+      data: {
+        pageTitle: 'Full-Time Hamburger Menu',
+        access: 'public',
+        bodyClass: 'hamburger hamburger-white'
+      }
+    })
+
+    .state('hamburger.three', {
+      url: '/hamburger/three',
+      templateUrl: 'modules/hamburger/templates/hamburger-three.html',
+      data: {
+        pageTitle: 'Full-Time Hamburger Menu - three',
+        access: 'public',
+        bodyClass: 'hamburger hamburger-white-filled'
+      }
+    })
+
+    .state('hamburger.four', {
+      url: '/hamburger/four',
+      templateUrl: 'modules/hamburger/templates/hamburger-four.html',
+      data: {
+        pageTitle: 'Full-Time Hamburger Menu - Four',
+        access: 'public',
+        bodyClass: 'hamburger hamburger-white-open'
+      }
+    })
+
+    .state('hamburger.five', {
+      url: '/hamburger/five',
+      templateUrl: 'modules/hamburger/templates/hamburger-five.html',
+      data: {
+        pageTitle: 'Full-Time Hamburger Menu',
+        access: 'public',
+        bodyClass: 'hamburger hamburger-white-open'
+      }
+    })
+
+    .state('hamburger.six', {
+      url: '/hamburger/six',
+      templateUrl: 'modules/hamburger/templates/hamburger-six.html',
+      data: {
+        pageTitle: 'Full-Time Hamburger Menu',
+        access: 'public',
+        bodyClass: 'hamburger hamburger-white-open'
+      }
+    })
+
+    .state('hamburger.seven', {
+      url: '/hamburger/seven',
+      templateUrl: 'modules/hamburger/templates/hamburger-seven.html',
+      data: {
+        pageTitle: 'Full-Time Hamburger Menu',
+        access: 'public',
+        bodyClass: 'hamburger hamburger-white-open'
+      }
+    });
+
+
+  });
+
+})();
