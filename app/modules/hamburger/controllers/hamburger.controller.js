21086e5237cb142ed8435962e0ea5eaf769e2ad8
Steve Lombardi <smlombardi@gmail.com>
2016-02-09 13:38:24 -0500

    prep for burger section
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/hamburger/controllers/hamburger.controller.js
+++ app/modules/hamburger/controllers/hamburger.controller.js
@@ -2,7 +2,6 @@
 
   'use strict';
 
-<<<<<<< f7d819aa25f0227aa9da8664def14ee5bc033900
   angular.module('pb.ds.splitview').controller('HamburgerController', function($log) {
 
     var _this = this;
@@ -87,79 +86,6 @@
   angular.element(document).ready(function() {
 
     c = angular.element(document.querySelector('#controller-demo')).scope();
-=======
-  angular.module('pb.ds.splitview').controller('SplitviewController', function($log) {
-
-    var _this = this;
-
-    _this.leftnav =  [{
-       'label': 'Appetizers',
-       'value': '',
-       'toggle': 'dropdown',
-       'children': [{
-         'label': 'Hot Wings',
-         'value': ''
-       }, {
-         'label': 'Nachos',
-         'value': ''
-       }, {
-         'label': 'Bruscheta',
-         'value': ''
-       }, {
-         'label': 'Chicken Fingers',
-         'value': ''
-       }]
-     }, {
-       'label': 'Salads',
-       'value': '',
-       'toggle': 'dropdown',
-       'children': [{
-         'label': 'Cobb',
-         'value': ''
-       }, {
-         'label': 'Chef',
-         'value': ''
-       }, {
-         'label': 'American',
-         'value': ''
-       }, {
-         'label': 'Greek',
-         'value': '',
-         'className': 'popovers'
-       }, {
-         'label': 'Tomato Basil',
-         'value': ''
-       }]
-     }, {
-       'label': 'Desserts',
-       'value': '',
-       'toggle': 'dropdown',
-       'children': [{
-         'label': 'Ice Cream',
-         'value': ''
-       }, {
-         'label': 'Rice Pudding',
-         'value': ''
-       }, {
-         'label': 'Creme Brulee',
-         'value': ''
-       }, {
-         'label': 'Sorbet',
-         'value': ''
-       }, {
-         'label': 'Cake',
-         'value': ''
-       }, {
-         'label': 'Yodels',
-         'value': ''
-       }, {
-         'label': 'Cannoli',
-         'value': ''
-       }]
-     }];
-
-
->>>>>>> Added pageslide directive & burger menu section
   });
 
 })();
