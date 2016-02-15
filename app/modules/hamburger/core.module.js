52689eb84d39ee7f556dcda85bdd8a35d3ac4ee1
Steve Lombardi <smlombardi@gmail.com>
2016-02-15 11:16:40 -0500

    got burger app working
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

---
+++ app/modules/hamburger/core.module.js
@@ -0,0 +1,16 @@
+(function () {
+
+  'use strict';
+
+  angular.module('pb.core', [
+    'pb.ds.hamburger',
+    'pageslide-directive',
+    'ngCookies',
+    'ngAnimate',
+    'ngResource',
+    'ngSanitize',
+    'hljs',
+    'ui.router',
+  ]);
+
+})();
