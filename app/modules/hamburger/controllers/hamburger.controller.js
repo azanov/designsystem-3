8d2d1ea2ba0714934792ef2e78d8912ceba5dccd
Steve Lombardi <smlombardi@gmail.com>
2016-02-16 10:01:36 -0500

    angular work
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/hamburger/controllers/hamburger.controller.js
+++ app/modules/hamburger/controllers/hamburger.controller.js
@@ -2,87 +2,19 @@
 
   'use strict';
 
-  angular.module('pb.ds.splitview').controller('HamburgerController', function($log) {
+  angular.module('pb.ds.burger').controller('HamburgerController', function($log) {
 
     var _this = this;
-
-
-    _this.checked = false; // This will be binded using the ps-open attribute
 
     _this.toggle = function() {
       _this.checked = !_this.checked;
       $log.debug('clicked');
     };
 
-    _this.leftnav = [{
-      'label': 'Appetizers',
-      'value': '',
-      'toggle': 'dropdown',
-      'children': [{
-        'label': 'Hot Wings',
-        'value': ''
-      }, {
-        'label': 'Nachos',
-        'value': ''
-      }, {
-        'label': 'Bruscheta',
-        'value': ''
-      }, {
-        'label': 'Chicken Fingers',
-        'value': ''
-      }]
-    }, {
-      'label': 'Salads',
-      'value': '',
-      'toggle': 'dropdown',
-      'children': [{
-        'label': 'Cobb',
-        'value': ''
-      }, {
-        'label': 'Chef',
-        'value': ''
-      }, {
-        'label': 'American',
-        'value': ''
-      }, {
-        'label': 'Greek',
-        'value': '',
-        'className': 'popovers'
-      }, {
-        'label': 'Tomato Basil',
-        'value': ''
-      }]
-    }, {
-      'label': 'Desserts',
-      'value': '',
-      'toggle': 'dropdown',
-      'children': [{
-        'label': 'Ice Cream',
-        'value': ''
-      }, {
-        'label': 'Rice Pudding',
-        'value': ''
-      }, {
-        'label': 'Creme Brulee',
-        'value': ''
-      }, {
-        'label': 'Sorbet',
-        'value': ''
-      }, {
-        'label': 'Cake',
-        'value': ''
-      }, {
-        'label': 'Yodels',
-        'value': ''
-      }, {
-        'label': 'Cannoli',
-        'value': ''
-      }]
-    }];
+
 
 
   });
 
-   });
 
 })();
