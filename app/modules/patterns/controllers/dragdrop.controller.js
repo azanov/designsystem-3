007c224d51118a345e3a93b563b3d0ac574bc407
danielcollette <daniel.collette@pb.com>
2016-03-03 08:13:14 -0500

    experimenting with styling

--- app/modules/patterns/controllers/dragdrop.controller.js
+++ app/modules/patterns/controllers/dragdrop.controller.js
@@ -5,7 +5,7 @@
   angular.module('pb.ds.patterns').controller('DragDropController', function($log, $scope) {
 
     var _this = this;
-    
+
     _this.items = {
       unselected: [
         {
@@ -28,26 +28,41 @@
         }
       ]
     };
-    
+
     _this.tooltip = {
       isEnabled: true
     };
-    
+
+    _this.isDragging = false;
+
     $scope.$on('first-bag.drag', function(e, el, container) {
       _this.tooltip.isEnabled = !_this.tooltip.isEnabled;
+      _this.isDragging = true;
+      $log.debug(e, el, container);
+      // if(!el.hasClass('dragging')){
+      //     el.addClass('dragging');
+      // }
+
     });
-    
+
     $scope.$on('first-bag.drop', function(e, el, container) {
       _this.tooltip.isEnabled = !_this.tooltip.isEnabled;
+      _this.isDragging = false;
+      // el.removeClass('dragging');
     });
-    
-    // $scope.$on('first-bag.over', function(e, el, container) {
-    //   el.addClass('over');
+
+    // $scope.$on('first-bag.cloned', function(el, clone, original, type) {
+    //   $log.debug(arguments);
+    //   el.addClass('cloned');
     // });
-    
-    // $scope.$on('first-bag.out', function(e, el, container) {
-    //   el.removeClass('over');
-    // });
+
+    $scope.$on('first-bag.over', function(e, el, container) {
+      el.addClass('over');
+    });
+
+    $scope.$on('first-bag.out', function(e, el, container) {
+      el.removeClass('over');
+    });
 
   });
 
