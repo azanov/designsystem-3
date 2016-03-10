4944a3e9983126dc833aa58e26feb9b9cb3d4f0c
Steve Lombardi <smlombardi@gmail.com>
2016-03-10 09:33:07 -0500

    fixed missing icons
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/foundation/controllers/foundation-iconography.controller.js
+++ app/modules/foundation/controllers/foundation-iconography.controller.js
@@ -51,11 +51,7 @@
       'ui-1_bold-delete',
       'ui-1_bold-remove',
       'ui-1_bookmark-add',
-      'ui-1_bookmark-remove',
-      'ui-1_bookmark-calendar-57',
-      'ui-1_bookmark-calendar-60',
-      'ui-1_bookmark-calendar-59',
-      'ui-1_bookmark-calendar-62'
+      'ui-1_bookmark-remove'
     ];
 
   });
