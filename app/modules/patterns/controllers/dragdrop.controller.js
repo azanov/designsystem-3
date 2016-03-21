aa4ffac363e3f4e625fd0f2c182c95064aa4b1d5
Steve Lombardi <smlombardi@gmail.com>
2016-03-21 13:00:27 -0400

    created container hovers
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/patterns/controllers/dragdrop.controller.js
+++ app/modules/patterns/controllers/dragdrop.controller.js
@@ -7,26 +7,20 @@
     var _this = this;
 
     _this.items = {
-      unselected: [
-        {
-          id: 0,
-          label: 'Nulla consequat malesuada quam. Proin quis sem sit amet sem malesuada tincidunt.'
-        },
-        {
-          id: 1,
-          label: 'Vestibulum luctus malesuada nisl, sit amet pharetra lorem malesuada faucibus. Donec ut diam imperdiet, pharetra lacus at, auctor sapien. Proin in imperdiet nulla.'
-        },
-        {
-          id: 2,
-          label: 'Donec mauris ante, scelerisque sed nunc eu, facilisis rutrum nulla.'
-        }
-      ],
-      selected: [
-        {
-          id: 3,
-          label: 'Integer condimentum eget lectus eu aliquam. Morbi at neque velit. Nullam faucibus elit sit amet facilisis faucibus. Maecenas nisl arcu, luctus non orci sed.'
-        }
-      ]
+      unselected: [{
+        id: 0,
+        label: 'Nulla consequat malesuada quam. Proin quis sem sit amet sem malesuada tincidunt.'
+      }, {
+        id: 1,
+        label: 'Vestibulum luctus malesuada nisl, sit amet pharetra lorem malesuada faucibus. Donec ut diam imperdiet, pharetra lacus at, auctor sapien. Proin in imperdiet nulla.'
+      }, {
+        id: 2,
+        label: 'Donec mauris ante, scelerisque sed nunc eu, facilisis rutrum nulla.'
+      }],
+      selected: [{
+        id: 3,
+        label: 'Integer condimentum eget lectus eu aliquam. Morbi at neque velit. Nullam faucibus elit sit amet facilisis faucibus. Maecenas nisl arcu, luctus non orci sed.'
+      }]
     };
 
     _this.tooltip = {
@@ -62,6 +56,14 @@
 
     $scope.$on('first-bag.out', function(e, el, container) {
       el.removeClass('over');
+    });
+
+    $scope.$on('first-bag.over', function(e, el, container) {
+      container.addClass('ex-over');
+    });
+
+    $scope.$on('first-bag.out', function(e, el, container) {
+      container.removeClass('ex-over');
     });
 
   });
