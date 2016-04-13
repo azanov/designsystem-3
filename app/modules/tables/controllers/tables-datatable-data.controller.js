facb72e709bfcccfc321dc2caa52b497101da34f
Steve Lombardi <smlombardi@gmail.com>
2016-04-13 14:18:54 -0400

    update table column header
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/tables/controllers/tables-datatable-data.controller.js
+++ app/modules/tables/controllers/tables-datatable-data.controller.js
@@ -1,50 +1,60 @@
 (function () {
   'use strict';
 
-  angular.module('pb.ds.tables').controller('TablesDataController',
-    function ($log) {
-      var _this = this;
+  angular.module('pb.ds.tables').controller('TablesDataController', function ($log, $uibModal, $scope, $sce) {
+    var _this = this;
 
-      _this.rowdata = [
+    // popovers
+    _this.dynamicPopover = {
+      content: 'Hello, World!',
+      templateUrl: 'modules/help/templates/popover_template.html',
+      title: 'Title'
+    };
+
+    _this.dynamicTooltip = 'Hello, World!';
+    _this.dynamicTooltipText = 'dynamic';
+    _this.htmlTooltip = $sce.trustAsHtml("I've been made <b>bold</b>!");
+
+    _this.rowdata = [
+      {
+        date: 'Date',
+        recipient: 'Recipient',
+        carrier: 'Carrier',
+        shipping: 'Shipping',
+        tracking: 'Tracking',
+        status: 'Status',
+        amount: 'Amount'
+      },
+      {
+        date: 'Date',
+        recipient: 'Recipient',
+        carrier: 'Carrier',
+        shipping: 'Shipping',
+        tracking: 'Tracking',
+        status: 'Status',
+        amount: 'Amount'
+      }
+    ];
+
+    _this.rowclick = function (item, event) {
+      event.preventDefault();
+      event.stopPropagation();
+      item.selected = !item.selected;
+    };
+
+    _this.xeditable = {
+      data: [
         {
-          date: 'Date',
-          recipient: 'Recipient',
-          carrier: 'Carrier',
-          shipping: 'Shipping',
-          tracking: 'Tracking',
-          status: 'Status',
-          amount: 'Amount'
+          first_name: 'John',
+          last_name: 'Smth',
+          carrier: 'USPS'
         },
         {
-          date: 'Date',
-          recipient: 'Recipient',
-          carrier: 'Carrier',
-          shipping: 'Shipping',
-          tracking: 'Tracking',
-          status: 'Status',
-          amount: 'Amount'
+          first_name: 'Susie',
+          last_name: 'Queue'
         }
-      ];
-
-      _this.rowclick = function (item, event) {
-        event.preventDefault();
-        event.stopPropagation();
-        item.selected = !item.selected;
-      };
-
-      _this.xeditable = {
-        data: [
-          {
-            first_name: 'John',
-            last_name: 'Smth',
-            carrier: 'USPS'
-          },
-          {
-            first_name: 'Susie',
-            last_name: 'Queue'
-          }
-        ],
-        carriers: ['USPS', 'FedEx']
-      };
-    });
+      ],
+      carriers: ['USPS', 'FedEx']
+    };
+  });
 })();
