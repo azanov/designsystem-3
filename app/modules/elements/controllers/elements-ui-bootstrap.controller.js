7dd854eb5a220562c2fc16642837bc990a5e9c5b
Steve Lombardi <smlombardi@gmail.com>
2016-04-20 12:13:09 -0400

    adding alerts
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/elements/controllers/elements-ui-bootstrap.controller.js
+++ app/modules/elements/controllers/elements-ui-bootstrap.controller.js
@@ -30,6 +30,20 @@
       }
     ];
 
+    // alerts
+    _this.alerts = [
+      { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+      { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+    ];
+
+    _this.addAlert = function () {
+      _this.alerts.push({msg: 'Another alert!'});
+    };
+
+    _this.closeAlert = function (index) {
+      _this.alerts.splice(index, 1);
+    };
+
     // modals
     _this.modals = {
       simple: function () {
