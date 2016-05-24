79ad5abb7ba84a72af9ae794194f47692dee9e2e
danielcollette <daniel.collette@pb.com>
2016-05-24 12:29:17 -0400

    initial cleanup

--- app/modules/account/controllers/onboarding.controller.js
+++ app/modules/account/controllers/onboarding.controller.js
@@ -1,11 +1,11 @@
 (function () {
   'use strict';
-  angular.module('pb.ds.patterns').controller('onBoardingCtrl', ['$log', '$scope', 'MockDataFactory',
-    function ($log, $scope, MockDataFactory) {
+  angular.module('pb.ds.patterns').controller('onBoardingCtrl',
+    function ($log, MockDataFactory) {
       var _this = this;
 
       MockDataFactory.get({filename: 'onboarding'}).$promise.then(function (data) {
         _this.slides = data.slides;
       });
-    }]);
+    });
 })();
--- app/modules/account/directives/onboarding.directive.js
+++ app/modules/account/directives/onboarding.directive.js
@@ -2,7 +2,9 @@
   'use strict';
 
   angular.module('app').directive('pbOnboarding', function () {
-    var link = function (scope, element, attr, controller) {};
+    var link = function (scope, element, attr, controller) {
+      console.log(scope, element, attr, controller);
+    };
 
     return {
       restrict: 'AE',
@@ -16,41 +18,44 @@
       return {
         restrict: 'A',
         link: function (scope, element, attrs, controllers) {
-          element.bind('click', function () {
-            var $drawer = $('.onboarding');
-            var $drawerContent = $drawer.find('.onboarding__content');
-            var drawerIsOpen = scope.onboardOpened;
-            var headerHeight = $('header').height();
-            var drawerOffsetTop = $drawer.offset().top;
-            var val = -(drawerOffsetTop - headerHeight);
-            // var drawerContentHeight = $('.drawer__content').find('.container-fluid').height();
-            var drawerContentHeight = footerPush();
+          // console.log(scope, element, attrs, controllers);
 
-            $drawer.css('top', val);
-            $drawer.toggleClass('open');
-            $drawerContent.css({'margin-bottom': val});
+          element.bind('click', function (e) {
+            // var $drawer = $('.onboarding');
+            // var $drawerContent = $drawer.find('.onboarding__content');
+            // var drawerIsOpen = scope.onboardOpened;
+            // //var headerHeight = $('header').height();
+            // var drawerOffsetTop = $drawer.offset().top;
+            // var val = -(drawerOffsetTop);
+            // var val = -(drawerOffsetTop - headerHeight);
+            // var drawerContentHeight = $('.drawer__content').find('.container-fluid').height();
+            // var drawerContentHeight = footerPush();
+
+            // $drawer.css('top', val);
+            // $drawer.toggleClass('open');
+            // $drawerContent.css({'margin-bottom': val});
+
             $('body', 'html').scrollTop(0);
 
-
-            function footerPush () {
-              // check if footer is not at the bottom
-              var $footer = $('footer');
-              var windowHieght = $(window).height();
-              var headerHeight = $('header').outerHeight(true);
-              var footerHeight = $footer.outerHeight(true);
-              var footerOffsetTop = $footer.offset().top;
-              var drawerNavHeight = $('.onboarding__navbar').outerHeight(true);
-              var drawerContentHeight = null;
-
-              // expand drawer content to fill screen height - footer height
-              if (footerOffsetTop > windowHieght || (footerOffsetTop + footerHeight) > windowHieght) {
-                drawerContentHeight = $('.onboarding__content').find('.container-fluid').height();
-              } else {
-                drawerContentHeight = windowHieght - headerHeight - footerHeight - drawerNavHeight;
-              }
-
-              return drawerContentHeight;
-            }
+            // function footerPush () {
+            //   // check if footer is not at the bottom
+            //   var $footer = $('footer');
+            //   var windowHieght = $(window).height();
+            //   //var headerHeight = $('header').outerHeight(true);
+            //   var footerHeight = $footer.outerHeight(true);
+            //   var footerOffsetTop = $footer.offset().top;
+            //   var drawerNavHeight = $('.onboarding__navbar').outerHeight(true);
+            //   var drawerContentHeight = null;
+            //
+            //   // expand drawer content to fill screen height - footer height
+            //   if (footerOffsetTop > windowHieght || (footerOffsetTop + footerHeight) > windowHieght) {
+            //     drawerContentHeight = $('.onboarding__content').find('.container-fluid').height();
+            //   } else {
+            //     drawerContentHeight = windowHieght - footerHeight - drawerNavHeight;
+            //   }
+            //
+            //   return drawerContentHeight;
+            // }
           });
         }
       };
