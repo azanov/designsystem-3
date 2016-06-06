863e71d0b559b8bfa80a247feb5dbc0527f4ac62
Steve Lombardi <smlombardi@gmail.com>
2016-06-06 10:30:57 -0400

    fixed input route to include resolves
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- app/modules/inputs/inputs.routes.js
+++ app/modules/inputs/inputs.routes.js
@@ -7,6 +7,18 @@
       templateUrl: 'modules/inputs/inputs-index.html',
       // abstract: true,
       controller: 'InputsController as inputs',
+      resolve: {
+        PeopleResolve: function ($log, MockDataFactory) {
+          return MockDataFactory.query({
+            filename: 'people_10'
+          });
+        },
+        CountriesResolve: function ($log, MockDataFactory) {
+          return MockDataFactory.query({
+            filename: 'countries'
+          });
+        }
+      },
       data: {
         pageTitle: 'Input Fields'
       }
