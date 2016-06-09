5c87ea264b94f17af7e513657e5f0558db5f4535
danielcollette <daniel.collette.pb@gmail.com>
2016-06-09 19:29:41 -0400

    slight refactor, remove service

--- app/modules/patterns/controllers/getting-started.controller.js
+++ app/modules/patterns/controllers/getting-started.controller.js
@@ -1,25 +1,32 @@
 (function () {
   'use strict';
 
-  angular.module('pb.ds.patterns').controller('GettingStartedController', function ($log, uiTourService) {
+  angular.module('pb.ds.patterns').controller('GettingStartedController', function ($log) {
     var _this = this;
     _this.tour;
+    
+    // `Launch Tour` button state
     _this.showButton = false;
 
+    // `Launch Tour` button
     _this.launchTour = function () {
       _this.showButton = false;
       _this.tour.start();
     };
 
+    // run the tour when ready
     _this.onReady = function (tour) {
-      _this.tour = uiTourService.getTour();
+      _this.tour = tour;
 
+      // show launch button when tour ends
       _this.tour.on('ended', function () {
         _this.showButton = true;
       });
 
-      tour.start();
+      // we're ready, start the show...
+      _this.tour.start();
     };
 
   });
+
 })();
