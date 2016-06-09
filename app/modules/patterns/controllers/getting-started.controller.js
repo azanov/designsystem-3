c87f2a9d1c9cd10f6ef8954d3c9e8216ebce9379
danielcollette <daniel.collette.pb@gmail.com>
2016-06-09 18:52:20 -0400

    add button

--- app/modules/patterns/controllers/getting-started.controller.js
+++ app/modules/patterns/controllers/getting-started.controller.js
@@ -4,18 +4,20 @@
   angular.module('pb.ds.patterns').controller('GettingStartedController', function ($log, uiTourService) {
     var _this = this;
     _this.tour;
+    _this.showButton = false;
 
-    _this.onTourStart = function (tour) {
-      console.log(tour);
+    _this.launchTour = function () {
+      _this.showButton = false;
+      _this.tour.start();
     };
-
-    _this.test = function (msg) {
-      console.log(msg);
-    }
 
     _this.onReady = function (tour) {
       _this.tour = uiTourService.getTour();
-      console.log('TOUR: ', _this.tour);
+
+      _this.tour.on('ended', function () {
+        _this.showButton = true;
+      });
+
       tour.start();
     };
 
